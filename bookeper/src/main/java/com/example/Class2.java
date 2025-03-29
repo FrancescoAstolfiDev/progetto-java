fd29a69476cc023139cadda8cd5bb258afee9aba
Francesco Astolfi <francescoastolfi@MacBook-Air-di-Francesco.fritz.box>
2025-03-29 13:49:58 +0100

    check

--- bookeper/src/main/java/com/example/Class2.java
+++ bookeper/src/main/java/com/example/Class2.java
@@ -2,6 +2,6 @@ package com.example;
 public class Class2 {
     public static void  funcClass( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World2!" );
     }
 }
